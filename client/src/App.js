43cd233c8e40e2e7ceaddc6a84dffa59ba693d47
robinmahto <robinmahto413@gmail.com>
2023-01-02 22:30:58 +0530

    client folder structure created

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+       <h1>Ecommerce Websites</h1>
+    </div>
+  );
+}
+
+export default App;
