2e3d4b31c1a169d846e1819991f79fd8657ef317
robinmahto <robinmahto413@gmail.com>
2023-01-03 00:12:37 +0530

    errorpage, homepages and router added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Home, ErrorPage } from './pages';
 import './App.css';
+
+const router = createBrowserRouter([{path: '/', element: <Home/>, errorElement: <ErrorPage/>}])
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
-       <h1>Ecommerce Websites</h1>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ client/src/pages/ErrorPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div className='bg-red-800 w-screen h-screen flex items-center justify-center'>
+        <h1 className='text-5xl text-white font-bold'>404 Not Found</h1>
+    </div>
+  )
+}
+
+export default ErrorPage
